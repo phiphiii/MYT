e0f74fcacf7902b3336fa40087c0c7fc9c73022d
xkoza <62543350+xKoZa@users.noreply.github.com>
2021-12-12 00:37:59 +0100

    Added shadow and outline options

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function dropShadow(){
+    var dropshadow = document.getElementById("dropshadow");
+    var text = document.getElementById("hotrod");
+    if(dropshadow.checked == true){
+        text.classList.add("drop_shadow");
+    }
+    else{
+        text.classList.remove("drop_shadow");
+    }
+}
+function textoutline(){
+    var textoutline = document.getElementById("textoutline");
+    var text = document.getElementById("hotrod");
+    if(textoutline.checked == true){
+        text.classList.add("textoutline");
+    }
+    else{
+        text.classList.remove("textoutline");
+    }
+}
