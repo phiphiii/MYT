93969045360405c074feef10be58da17cd1acf77
xkoza <62543350+xKoZa@users.noreply.github.com>
2021-12-12 16:28:22 +0100

    added some more options to menu bar

--- javascript.js
+++ javascript.js
@@ -12,13 +12,31 @@ function textoutline(){
     var textoutline = document.getElementById("textoutline");
     var text = document.getElementById("hotrod");
     if(textoutline.checked == true){
-        text.classList.add("textoutline");
+        text.classList.add("text_outline");
     }
     else{
-        text.classList.remove("textoutline");
+        text.classList.remove("text_outline");
+    }
+}
+function textcenter(){
+    var textcenter = document.getElementById("textcenter");
+    var text = document.getElementById("maintextdiv");
+    if(textcenter.checked == true){
+        text.classList.add("text_center");
+    }
+    else{
+        text.classList.remove("text_center");
     }
 }
 function udpdateFontSize(){
     var fontsize = document.getElementById("fontSizeChange").value;
-    document.getElementById("hotrod").style.fontSize = ""+fontsize+"px";
+    document.getElementById("hotrod").style.fontSize = fontsize+"px";
+}
+function udpdateBgColor(){
+    var bgcolor = document.getElementById("bgColor").value;
+    document.getElementById("maintextdiv").style.backgroundColor = bgcolor;
+}
+function udpdateTxtColor(){
+    var txtcolor = document.getElementById("txtColor").value;
+    document.getElementById("hotrod").style.color = txtcolor;
 }
